9ef84efc94e7462f59957f00d62f75128fbadd9b
Anjela <petrosyan.anjela.1994@gmail.com>
2022-03-03 20:12:29 +0400

    day20

---
+++ src/HomeWorks/day20/task4/Shooter.java
@@ -0,0 +1,9 @@
+package HomeWorks.day20.task4;
+
+public class Shooter  extends Warrior{
+
+    @Override
+    protected int hit() {
+        return 0;
+    }
+}
---
+++ src/HomeWorks/day20/task4/SwordsMen.java
@@ -0,0 +1,8 @@
+package HomeWorks.day20.task4;
+
+public class SwordsMen extends Warrior{
+    @Override
+    protected int hit() {
+        return 0;
+    }
+}
