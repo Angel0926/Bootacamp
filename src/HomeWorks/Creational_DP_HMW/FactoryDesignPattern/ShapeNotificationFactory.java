d4031987963d1ab4091c55c41d7d0403c44955e9
Anjela <petrosyan.anjela.1994@gmail.com>
2022-04-26 16:47:07 +0400

    Creational_DP

--- src/HomeWorks/Creational_DP_HMW/FactoryDesignPattern/ShapeNotificationFactory.java
+++ src/HomeWorks/Creational_DP_HMW/FactoryDesignPattern/ShapeNotificationFactory.java
@@ -3,6 +3,7 @@ package HomeWorks.Creational_DP_HMW.FactoryDesignPattern;
 import HomeWorks.Creational_DP_HMW.FactoryDesignPattern.models.Notification;
 
 public abstract class ShapeNotificationFactory {
+
     public void GetDrawNotification(){
         Notification notification=createNotification();
         notification.GetShape();
